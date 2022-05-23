261ed14e13a7a5c30d9bef3e1d9ffb80b1f60f2c
ravinani04 <g.ravikumar72@gmail.com>
2022-05-22 20:30:19 -0700

    changes done in app

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask_restful import Api, Resource
 from logic import Add, Subtract, Multiply, Divide
 
 
-app1 = Flask(__name__)
-api = Api(app1)
+app = Flask(__name__)
+api = Api(app)
 
 api.add_resource(Add, "/add")
 api.add_resource(Subtract, "/subtract")
@@ -12,13 +12,34 @@ api.add_resource(Multiply, "/multiply")
 api.add_resource(Divide, "/division")
 
 
-@app1.route('/')
+@app.route('/')
 def hello_world():
     return "Hello World!"
 
 
 if __name__=="__main__":
     app1.run(host="0.0.0.0")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
